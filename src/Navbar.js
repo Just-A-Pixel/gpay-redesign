f38cd6d69b49aaf3a1713f469643025c16aaaaaf
Just-A-Pixel <Raj.Anand0511@gmail.com>
2021-05-31 23:09:06 +0530

    minor

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,9 +5,7 @@ function Navbar({dark}) {
     console.log(dark)
     return (
         <div className="navbar"  style={dark?{background:'black', color:'white'}:{}}>
-            <li>
-                Home
-            </li>
+           
             <li>
             <Link to="passbook" style={{ textDecoration: 'none' }} ><li style={{ textDecoration: 'none' }} >Passbook</li></Link>
             </li>
@@ -17,7 +15,7 @@ function Navbar({dark}) {
             <Link to="/pay" style={{ textDecoration: 'none' }}><li className='navbar__pay'>Pay</li></Link>
             <li>Rewards</li>
             <li>Profile</li>
-            <li>More</li>
+          
         </div>
     )
 }
